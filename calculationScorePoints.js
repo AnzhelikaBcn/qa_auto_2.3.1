9886e33205ad168e160d52d191f41777f78d61d5
ANZHELIKA <angela.pikulina@gmail.com>
2023-10-29 22:34:21 +0100

    HW-2-3-all

--- calculationScorePoints.js
+++ calculationScorePoints.js
@@ -1,3 +1,17 @@
+/*
+Тестовые данные — дан массив объектов players.
+
+Задача: вывеcти в консоль игрока с максимальным значением scorePoints. 
+Использовать цикл, обращение к свойству через точку и метод глобального 
+объекта Math, позволяющий найти большее число из всех возможных.
+
+
+1. Для начала создать пустой массив для хранения очков
+2. Используя цикл, заполнить новый массив значениями очков из данного вам массива players
+здесь будет удобно использовать цикл for..in. 
+*/
+
+
 const players = [
     {
       "id": 1,
@@ -25,14 +39,24 @@ const players = [
   const scores = [];
   
   // Заполнение массив значениями очков из массива players
-  for (let i = 0; i < players.length; i++) {
-    scores.push(players[i].scorePoints);
+//   Обычный цикл:
+//   for (let i = 0; i < players.length; i++) {
+//     scores.push(players[i].scorePoints);
+//   }
+
+//   Цикл for in
+  for (const player of players) {
+    for (const key in player) {
+      if (key === "scorePoints") {
+        scores.push(player[key]);
+      }
+    }
   }
   
   // Использование объекта Math и метода Math.max для поиска максимального значения
   const maxScore = Math.max(...scores);
   
-  // Найхождение игрока с максимальным значением scorePoints
+  // Нахождение игрока с максимальным значением scorePoints
   const playerWithMaxScore = players.find(player => player.scorePoints === maxScore);
   
   console.log("Игрок с максимальным значением scorePoints:");
